eae3da21f7e62b85e8c1d86d071bc0bce7c28af6
lt-winter <minhdong9304@gmail.com>
2026-03-22 16:03:03 +0700

    feat(user): add auto admin creation and duplicate email check

--- src/main/java/com/winter/quiz/repository/UserRepository.java
+++ src/main/java/com/winter/quiz/repository/UserRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.winter.quiz.entity.User;
+import com.winter.quiz.enums.UserRole;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByRole(UserRole role);
 
+    User findFirstByEmail(String email);
 }
--- src/main/java/com/winter/quiz/service/user/UserServiceImpl.java
+++ src/main/java/com/winter/quiz/service/user/UserServiceImpl.java
@@ -1,8 +1,35 @@
 package com.winter.quiz.service.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.winter.quiz.entity.User;
+import com.winter.quiz.enums.UserRole;
+import com.winter.quiz.repository.UserRepository;
+
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
 
+    @PostConstruct
+    private void createAdminUser() {
+        User optionalUser = userRepository.findByRole(UserRole.ADMIN);
+        if (optionalUser == null) {
+            User user = new User();
+
+            user.setName("Admin");
+            user.setEmail("admin@gmail.com");
+            user.setRole(UserRole.ADMIN);
+            user.setPassword("admin123");
+
+            userRepository.save(user);
+        }
+    }
+
+    public Boolean hasUserWithEmail(String email) {
+        return userRepository.findFirstByEmail(email) != null;
+    }
 }
